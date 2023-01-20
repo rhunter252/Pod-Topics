9ab4e2865d56ed16beca0711ae76007826996be7
Randy Hunter <randyh252@gmail.com>
2023-01-20 18:59:04 -0500

    working on server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,29 @@
 import express from "express";
 const app = express();
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
 const port = process.env.PORT || 5000;
+dotenv.config();
+
+mongoose.set("strictQuery", false);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.VITE_MONGO);
+    console.log("connected to mongdb");
+  } catch (error) {
+    throw error;
+  }
+};
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send(process.env.VITE_MONGO);
 });
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => {
+  connect();
+  console.log(`Server started on port ${port}`);
+});

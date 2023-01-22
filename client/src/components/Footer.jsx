c1466248aa87c7c4b86afe21257fca908bcd5e78
Randy Hunter <randyh252@gmail.com>
2023-01-22 15:45:44 -0500

    added contact form

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,120 @@
-import React from "react";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Footer = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_ei9ad5i",
+        "template_tjkl4wq",
+        form.current,
+        "Vwav1N79Ut6aGRsWP"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
+
   return (
-    <footer className="py-2 bg-blue-600 flex place-content-center items-center">
-      <div className="mx-auto text-center">
+    <footer className="block p-6 rounded-lg shadow-lg bg-blue-500 w-full ">
+      <div className="mx-auto max-w-3xl">
+        <p className="text-lg">Have an idea for a interesting topic?</p>
+        <form className="flex flex-col pt-4" ref={form} onSubmit={sendEmail}>
+          <label>Message</label>
+          <textarea
+            className="
+        block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            name="message"
+          />
+          <div className="pt-4">
+            <label>Name</label>
+            <input
+              className="form-control block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        mb-4
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              type="text"
+              name="user_name"
+            />
+            <label>Email</label>
+            <input
+              className="form-control block w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              type="email"
+              name="user_email"
+            />
+          </div>
+          <button
+            className="w-24 mx-auto my-4
+            px-6
+            py-2.5
+            bg-blue-600
+            text-white
+            font-medium
+            text-xs
+            leading-tight
+            uppercase
+            rounded
+            shadow-md
+            hover:bg-blue-700 hover:shadow-lg
+            focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+            active:bg-blue-800 active:shadow-lg
+            transition
+            duration-150
+            ease-in-out"
+            type="submit"
+            value="Send"
+          >
+            Send
+          </button>
+        </form>
+      </div>
+      <div className="text-center text-amber-400 mt-4">
         &copy; {new Date().getFullYear()} Pod Topics. All rights reserved.
       </div>
     </footer>
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,34 +1,31 @@
 import { useState, useEffect } from "react";
-import Topics from "../constants/topics";
 import { client } from "../lib/client";
 
 const Main = () => {
   const [getTopic, setGetTopic] = useState("Tap the button. Get a topic.");
   const [tops, setTops] = useState([]);
 
-  const randomTopic = tops[Math.floor(Math.random() * tops.length)];
-
   const handleClick = () => {
     client
       .fetch(
         `*[_type == "topics"]{
-      title,
       description}`
       )
       .then((data) => {
         setTops(data);
         setGetTopic(data[Math.floor(Math.random() * data.length)].description);
-        console.log(data[Math.floor(Math.random() * data.length)].description);
       })
       .catch((error) => console.log(error));
   };
 
   return (
     <div className="bg-blue-100 h-screen">
-      <div className="container mx-auto grid h-full place-items-center sm:px-4">
-        <h2 className="text-3xl leading-normal">{getTopic}</h2>
+      <div className="container mx-auto grid h-screen place-items-center">
+        <h2 className="text-4xl lg:text-4xl px-4 text-center leading-normal">
+          {getTopic}
+        </h2>
         <button
-          className="rounded-full px-6 py-2 bg-blue-600 hover:bg-blue-700 text-blue-100"
+          className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-full text-lg px-12 py-6 text-center"
           onClick={handleClick}
         >
           Get a Topic
